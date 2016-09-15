91e24c7c6d02dcaacc1e09cd2898cd6a569ccfd4
mikey neilson <mikeyneilson@googlemail.com>
2016-09-15 21:43:25 +0100

    Added relationship between comments and products correct

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+	#--------------------------------------------------
+	# Validations
+	#--------------------------------------------------
+	validates :title, :description, :link, presence: true
+	has_many :comments
 end
